8d0a2f16b4c58ca79fdf52dd303d2ad2cd5a66d7
Nicole Zheng <zznicole@hotmail.com>
2020-04-11 01:20:28 +0200

    add 4 solve 2

--- src/script.js
+++ src/script.js
@@ -27,3 +27,38 @@ function repeatStr(str2, count) {
     return str2.repeat(count);
 }
 alert(repeatStr('Hiya', 5));
+
+
+// Write a function that accepts three separate digits and makes them a number. 
+// For example, 1, 4, and 9 will become 149.
+function threeNumber(number1, number2, number3) {
+    return number1 * 100 + number2 * 10 + number3;
+}
+
+alert(threeNumber(6, 8, 9));
+
+// Write a function that accepts a year and returns whether it is a leap year. 
+// A year is a leap year if it is (1) divisible by 4 and not divisible by 100, 
+// or else (2) is divisible by 400. It should return a boolean value.
+function leapYear(year) {
+    if (year % 4 == 0  && year % 100 !== 0 ) {
+        return 'It\'s a leap year!';
+    } else if  (year % 400 == 0) {
+        return false;
+    }
+}
+alert(leapYear(800));
+
+// Write a function that takes two parameters: the year of birth and the year to count years in relation to. 
+// Provide output in this format: For dates in the future: “You are NN years old.” For dates in the 
+// past: “You will be born in NN years.” If the year of birth equals the year requested 
+// return: “You were born this very year!” For example, for 2000 and 1998 you will get “You will be born in 2 years.”
+function whenYoubBorn(birthYear, year) {
+    birthyear = 1980;
+    if (year )
+}
+
+
+// Write a function that squares every digit in a number and returns a result. For example, 9119 will return 811181, 
+// because 9^2 is 81 and 1^2 is 1.
+function squareEveryDigit()
